0c49f6027fca81be55ce6109798b86f7ce16dc10
Tuon Bondol <bondol@2ntkh.com>
2017-07-27 00:44:02 +0700

    - Type and Type Inference

--- src/main/kotlin/com.tuonbondol.kotlin.main.kt
+++ src/main/kotlin/com.tuonbondol.kotlin.main.kt
@@ -1,4 +1,26 @@
-
-fun main(args:Array<String>){
+fun main(args: Array<String>) {
     println("Hello World!")
+
+    // Type Inference
+    val aSentence = "I'm a string"
+    val myPi = 3.14
+    val myAnswer = 42
+
+    var sString:String
+    var aDouble: Double
+    var aInt: Int
+
+    var myDouble = 1.999
+    var myFloat = 1.9F
+    var myLong = 1234L
+    var myInt = 12
+    var myShort:Short = 12
+    var myByte:Byte = 127
+
+    val aLongNumber = 123_456_789
+    val theSameNumber = 123456789
+
+    val anInt: Int = 1
+    val aLong:Long = anInt.toLong()
 }
+

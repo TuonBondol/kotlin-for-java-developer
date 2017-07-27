5b43dd188b0747715407746b98c8a79e3078753d
Tuon Bondol <bondol@2ntkh.com>
2017-07-28 00:49:13 +0700

    - Kotlin Funktionale Library

---
+++ src/main/kotlin/Functional.kt
@@ -0,0 +1,12 @@
+package com.tuonbondol.kotlin
+
+import org.funktionale.collections.destructured
+
+fun functional(){
+    val (head, tail) = listOf(1, 2, 3).destructured()
+    println("Head $head and tail = $tail")
+}
+
+fun main(args: Array<String>){
+    functional()
+}

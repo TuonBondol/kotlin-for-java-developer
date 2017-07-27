5ca83d00e1fec8d77d7c59b5042f37b9522a1fb0
Tuon Bondol <bondol@2ntkh.com>
2017-07-28 00:54:41 +0700

    - Kotlin Compose and Composition

--- src/main/kotlin/Functional.kt
+++ src/main/kotlin/Functional.kt
@@ -1,12 +1,25 @@
 package com.tuonbondol.kotlin
 
 import org.funktionale.collections.destructured
+import org.funktionale.composition.compose
+import org.funktionale.composition.forwardCompose
 
-fun functional(){
+fun functional() {
     val (head, tail) = listOf(1, 2, 3).destructured()
     println("Head $head and tail = $tail")
+
+    val add5 = { i: Int -> i + 5 }
+    val multiplyBy2 = {i: Int -> i * 2}
+
+    val muliplyBy2andAdd5 = add5 compose multiplyBy2
+    val composeResults = muliplyBy2andAdd5(10)
+    println("multiplyBy2andAdd5(10) = $composeResults")
+
+    val add5andMultiplyBy2 = add5 forwardCompose  multiplyBy2
+    val forwardComposeResults = add5andMultiplyBy2(10)
+    println("add5andMultiplyBy2 = $forwardComposeResults")
 }
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
     functional()
 }
